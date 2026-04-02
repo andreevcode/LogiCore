f5e0118f5b8be88ada6328986e3c446c14d12fe6
andreevcode <andreev.asu@yandex.ru>
2026-04-02 17:59:34 +0200

    LOGI-03: new dependencies for kafka

--- core-logistics/build.gradle.kts
+++ core-logistics/build.gradle.kts
@@ -12,12 +12,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
 
     implementation("org.springframework.kafka:spring-kafka")
+    // 1. Для автоподстановки пропертей в IDEA (Metadata)
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
 
     runtimeOnly("org.postgresql:postgresql")
     
     testImplementation(libs.bundles.spring.test.infrastructure)
+    testImplementation("org.springframework.kafka:spring-kafka-test")
 
     testRuntimeOnly(libs.junit.platform)
 }
